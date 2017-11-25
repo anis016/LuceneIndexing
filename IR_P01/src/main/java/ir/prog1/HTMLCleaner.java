e00f2bf03948b3af8f305e205503d503e3912559
Sayed Anisul Hoque <anis.cuet016@gmail.com>
2017-11-26 00:49:00 +0100

    HTML Cleaner file added

--- IR_P01/src/main/java/ir/prog1/HTMLCleaner.java
+++ IR_P01/src/main/java/ir/prog1/HTMLCleaner.java
@@ -1,4 +1,31 @@
 package ir.prog1;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class HTMLCleaner {
+
+    HTMLCleaner() {
+
+    }
+
+    public void getTitle(String ur) {
+
+    }
+
+    public static void main(String... args) {
+        String htmlString = "<html>" +
+                              "<head>" +
+                                "<title>My title</title>" +
+                              "</head>" +
+                              "<body>Body content</body>" +
+                            "</html>";
+
+        Document doc = Jsoup.parse(htmlString);
+        String title = doc.title();
+        String body  = doc.body().text();
+
+        System.out.printf("Title: %s\n", title);
+        System.out.printf("Body: %s\n", body);
+    }
 }
